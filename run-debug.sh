90af83ca0a87cb012eaa4c7697773dffa07d53e3
SevenMachines <SevenMachines@yahoo.co.uk>
2011-03-29 08:41:12 +0100

    * Upgrade the test client to something more useful

--- run-debug.sh
+++ run-debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-LD_LIBRARY_PATH=../cryomesh/Debug/:../cryomesh-common/Debug/ Debug/cryoterminal "localhost:10666"
+LD_LIBRARY_PATH=../cryomesh/Debug/:../cryomesh-common/Debug/ Debug/cryoterminal localhost 10666 "$1"
--- src/Main.cpp
+++ src/Main.cpp
@@ -28,15 +28,41 @@ Main::~Main() {
 }//NAMESPACE
 
 int main(int argc, char* argv[]) {
-	try {
-		if (argc != 2) {
-			std::cerr << "Usage: client <host>" << std::endl;
-			return 1;
+
+	// printout args
+	{
+		std::stringstream ssargs;
+		for (int i = 0; i < argc; i++) {
+			ssargs << argv[i];
+			if (i < argc) {
+				ssargs << " ";
+			}
 		}
+
+		std::cout << "main: " << ssargs.str() << std::endl;
+	}
+	// get and check commands
+	if (argc < 4) {
+		std::cerr << "Usage: cryoterminal <server> <port> <command>" << std::endl;
+		return 1;
+	}
+	std::string server_str(argv[1]);
+	std::string port_str(argv[2]);
+	std::stringstream ss;
+	for (int i = 3; i < argc; i++) {
+		ss << argv[i];
+		if (i < argc-1) {
+			ss << " ";
+		}
+	}
+	std::string command_str = ss.str();
+
+	try {
+
 		boost::asio::io_service io_service;
 
 		tcp::resolver resolver(io_service);
-		tcp::resolver::query query("localhost", "10666");
+		tcp::resolver::query query(server_str, port_str);
 		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 		tcp::resolver::iterator end;
 
@@ -51,10 +77,9 @@ int main(int argc, char* argv[]) {
 		}
 		for (;;) {
 			boost::array<char, 128> buf;
-			std::string bufout = "client sends a fuck you!";
-			boost::asio::write(socket, boost::asio::buffer(bufout));
+			boost::asio::write(socket, boost::asio::buffer(command_str));
 			size_t lenin = socket.read_some(boost::asio::buffer(buf), error);
-			std::cout <<  "CLIENT: recv: " << buf.data() << ":" << lenin << std::endl;
+			std::cout << "CLIENT: recv: " << buf.data() << ":" << lenin << std::endl;
 
 			if (error == boost::asio::error::eof)
 				break; // Connection closed cleanly by peer.
